b99b3152392e3068fff0a7f9c14f8232e655f00f
Danil <cruze20016@gmail.com>
2022-12-20 18:47:06 +0300

    New model (works incorrectly)

--- resourceTableModel.h
+++ resourceTableModel.h
@@ -3,41 +3,6 @@
 
 #include <QAbstractTableModel>
 
-//class resourceTableModel : public QAbstractTableModel
-//{
-//public:
-//    explicit resourceTableModel(int nRows, int nColumns, QObject *parent = nullptr);
-
-//private:
-//    int m_nRows;
-//    int m_nColumns;
-
-//protected:
-//    QHash<QModelIndex, QVariant> m_hash;
-
-//    // QAbstractItemModel interface
-//public:
-//    int rowCount(const QModelIndex &parent) const override;
-//    int columnCount(const QModelIndex &parent) const override;
-//    QVariant data(const QModelIndex &index, int role) const override;
-//    bool setData(const QModelIndex &index, const QVariant &value, int role) override;
-//};
-
-//class resourceData : public resourceTableModel
-//{
-//    Q_OBJECT
-
-//public:
-//    resourceData(int nRows, QObject *parent = nullptr);
-//    void setResource(int clientNum,int resourceType, QString course);
-
-//    // QAbstractItemModel interface
-//public:
-//    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
-//};
-
-//-------------------------------
-
 class resourceTableModel : public QAbstractTableModel
 {
 public:
